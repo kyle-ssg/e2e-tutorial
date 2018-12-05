da92f04983f923e0538094865ba0ca2b6e83fe13
Kyle Johnson <kyle@solidstategroup.com>
2018-12-05 16:40:06 +0000

    Update index.js

--- e2e/index.js
+++ e2e/index.js
@@ -1,10 +1,11 @@
 module.exports = Object.assign(
     {
-        before: (browser, done) => {
-            done();
+        before: (browser, done) => { // Runs before tests start
+            // runs before all of the tests run
+            done();  // tell nightwatch we're done after we have done all of our bootstrapping
         },
-        after: (browser, done) => {
-            // runs before all of the tests run, call done() when you're finished
+        after: (browser, done) => { // Runs after the tests have finished
+            // runs after all of the tests run
             browser.end(); // kill the browser
             done(); // tell nightwatch we're done
         },
