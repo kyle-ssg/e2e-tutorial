457b989c1f7a43574db53c9278b7d4e22b3a921e
kyle <kyle@solidstategroup.com>
2018-12-05 14:38:58 +0000

    Registration test

--- e2e/register.test.js
+++ e2e/register.test.js
@@ -1,4 +1,4 @@
-const { byTestID, gotoMailinator, getMailinatorMessage } = require('./util');
+const { byTestID } = require('./util');
 
 module.exports = (email, password) => ({
     'Registration - test page loads': function (browser) {
@@ -10,15 +10,5 @@ module.exports = (email, password) => ({
             .setValue(byTestID('registration-password'), password)
             .click(byTestID('registration-submit')) // submit form
             .waitForElementVisible(byTestID('check-email-page')); // wait for confirm email page
-    },
-    'Registration - Confirm email': function (browser) {
-        gotoMailinator(browser, email); // click first mailinator email
-        browser.waitForElementVisible('#inboxpane table tr td:nth-child(3n)')
-            .click('#inboxpane table tr td:nth-child(3n)');
-
-        getMailinatorMessage(browser) // get the message content
-            .getAttribute('a[target="_other"]', 'href', // get the first anchor and go to that url
-                result => browser.url(result.value))
-            .waitForElementVisible(byTestID('logged-in-page')); // wait for logged in page
     },
 });
--- e2e/util.js
+++ e2e/util.js
@@ -1,14 +1,3 @@
 module.exports = {
     byTestID: id => `[data-test="${id}"]`,
-    gotoMailinator(browser, email) {
-        const target = email.replace('@mailinator.com', '');
-        console.log(`https://www.mailinator.com/v3/index.jsp?zone=public&query=${target}#/#inboxpane`);
-        return browser.url(`https://www.mailinator.com/v3/index.jsp?zone=public&query=${target}#/#inboxpane`)
-            .waitForElementVisible('#inboxpane');
-    },
-    getMailinatorMessage(browser) {
-        return browser.waitForElementVisible('#msg_body')
-            .frame('msg_body')
-            .pause(1000)
-    },
 };
