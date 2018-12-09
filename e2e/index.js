a32ce8705753748cace62129e76ced387ef92e14
kyle <kyle@solidstategroup.com>
2018-12-09 23:17:49 +0000

    Confirm email test

--- e2e/index.js
+++ e2e/index.js
@@ -1,10 +1,10 @@
 module.exports = Object.assign(
     {
-        before: (browser, done) => { // Runs before tests start
+        before: (browser, done) => {
             // runs before all of the tests run
-            done();  // tell nightwatch we're done after we have done all of our bootstrapping
+            done(); // tell nightwatch we're done after we have done all of our bootstrapping
         },
-        after: (browser, done) => { // Runs after the tests have finished
+        after: (browser, done) => {
             // runs after all of the tests run
             browser.end(); // kill the browser
             done(); // tell nightwatch we're done
