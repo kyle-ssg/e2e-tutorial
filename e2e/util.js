bf9c8d013625c3aea344266ffa7efb7642809f13
kyle <kyle@solidstategroup.com>
2018-12-10 22:12:36 +0000

    update utils

--- e2e/util.js
+++ e2e/util.js
@@ -2,16 +2,18 @@ module.exports = {
     byTestID: id => `[data-test="${id}"]`,
     gotoMailinator(browser, email) {
         const target = email.replace('@mailinator.com', ''); // get the mailinator username
-        return browser.url(`https://www.mailinator.com/v3/index.jsp?zone=public&query=${target}#/#inboxpane`) // goto the
+        // goto the inbox and wait for the content to be ready
+        return browser.url(`https://www.mailinator.com/v3/index.jsp?zone=public&query=${target}#/#inboxpane`)
             .waitForElementVisible('#inboxpane');
     },
     clickFirstMailinatorMessage(browser) {
+        // click the latest message in the inbox pane
         browser.waitForElementVisible('#inboxpane table tr td:nth-child(3n)')
             .click('#inboxpane table tr td:nth-child(3n)');
     },
     getMailinatorMessage(browser) {
-        return browser.waitForElementVisible('#msg_body')
-            .frame('msg_body')
-            .pause(1000);
+        return browser.waitForElementVisible('#msg_body') // wait for the content to be ready
+            .frame('msg_body') // switch to the message content's iframe
+            .pause(1000); // the most reliable way I've found to ensure the content has loaded
     },
 };
